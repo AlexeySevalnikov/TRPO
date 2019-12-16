9fa8ecdd24c396327c58b3a4171054e0a9cefb56
AlexeySevalnikov <51491806+AlexeySevalnikov@users.noreply.github.com>
2019-12-16 09:22:43 +0200

    Add files via upload

---
+++ Unit1.cpp
@@ -0,0 +1,25 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+
+#include "Unit1.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.dfm"
+TForm1 *Form1;
+//---------------------------------------------------------------------------
+__fastcall TForm1::TForm1(TComponent* Owner)
+	: TForm(Owner)
+{
+
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::Button1Click(TObject *Sender)
+{
+	 Form1->Canvas->Brush->Color = clBlue;
+	 Form1->Canvas->Rectangle(0,10,20,20);
+	 Form1->Canvas->Brush->Color = clRed;
+	 Form1->Canvas->Pie(40,40, 80,80, 0,0, 80,80);
+}
+//---------------------------------------------------------------------------
